a707342771f4cef30362b3ddddb8015bbd6ed20f
Wenshuai Hou <wenhoujx@gmail.com>
2023-01-25 20:45:36 -0500

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,13 @@
         style="border-bottom: 5px solid var(--primary); margin-bottom: var(--spacing);"
     />
     <slot />
+    <footer
+        style="text-align: right; margin-top: var(--typography-spacing-vertical);"
+    >
+        <small>
+            Created by <a href="mailto:onepicodev@gmail.com">
+                onepicodev@gmail.com
+            </a>
+        </small>
+    </footer>
 </main>
--- src/routes/MyNav.svelte
+++ src/routes/MyNav.svelte
@@ -1,14 +1,22 @@
+<script>
+    import BoopAction from "$lib/components/BoopAction.svelte";
+</script>
+
 <nav>
     <ul>
-        <li>
-            <a href="/">
-                <img src="/goji.png" alt="goji blog" style="width:2rem" />
-                <h5 style="display: inline; color: #ff8087">GojiWrite</h5>
-            </a>
-        </li>
-        <li>
-            <a href="/post">帖子
-            </a>
-        </li>
+        <BoopAction boopParams={{ rotation: 10, timing: 200 }}>
+            <li>
+                <a href="/">
+                    <img src="/goji.png" alt="goji blog" style="width:2rem" />
+                    <h5 style="display: inline; color: #ff8087">GojiWrite</h5>
+                </a>
+            </li>
+        </BoopAction>
+
+        <BoopAction boopParams={{ rotation: 10, timing: 200 }}>
+            <li>
+                <a href="/post">帖子 </a>
+            </li>
+        </BoopAction>
     </ul>
 </nav>
